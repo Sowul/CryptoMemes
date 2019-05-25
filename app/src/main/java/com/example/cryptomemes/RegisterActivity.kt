f533fc66259a95c21e0148051d56500016c3bd74
Sowul <bartosz.sowul@pm.me>
2019-05-25 14:12:28 +0200

    Add register method using Firebase

--- app/src/main/java/com/example/cryptomemes/RegisterActivity.kt
+++ app/src/main/java/com/example/cryptomemes/RegisterActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -29,7 +31,22 @@ class RegisterActivity : AppCompatActivity() {
         val email = email_edittxt_reg.text.toString()
         val password = pass_edittxt_reg.text.toString()
 
-        Log.d(TAG, "Email: $email")
-        Log.d(TAG, "Password: $password")
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password field", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d(TAG, "Creating user with email: $email")
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d(TAG, "GOOD Created user with uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "BAD Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
