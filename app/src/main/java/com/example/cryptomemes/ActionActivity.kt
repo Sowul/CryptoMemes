37b394ebdf4d0fe29d868ac24b4f0e763c38574c
Sowul <bartosz.sowul@pm.me>
2019-05-25 21:06:09 +0200

    Add jpg filtering

--- app/src/main/java/com/example/cryptomemes/ActionActivity.kt
+++ app/src/main/java/com/example/cryptomemes/ActionActivity.kt
@@ -3,9 +3,14 @@ package com.example.cryptomemes
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_action.*
 
 class ActionActivity : AppCompatActivity() {
+
+    val TAG = "ActionActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,40 @@ class ActionActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
+        }
+
+        encrypt_btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+        decrypt_btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == android.support.v7.app.AppCompatActivity.RESULT_OK && data != null) {
+            val msgImgUri = data.data
+            val cr = this.contentResolver
+            val type = cr.getType(data.data)
+
+            if (type.split("/").last().replace("\"", "") == "png") {
+                Toast.makeText(this, "File should be in .jpg format", Toast.LENGTH_LONG).show()
+                Log.d(TAG, "msgImgUri: $msgImgUri")
+                Log.d(TAG, "mimeType: $type")
+                Log.d(TAG, "${type.split("/").last().replace("\"", "")}")
+                return
+            }
+
+            Log.d(TAG, "msgImgUri: $msgImgUri")
+            Log.d(TAG, "mimeType: $type")
+            Log.d(TAG, "${type.split("/").last().replace("\"", "")}")
         }
     }
 }
