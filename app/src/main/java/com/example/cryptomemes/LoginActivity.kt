2847dfead75386764e2e955faf124a20e238404b
Sowul <bartosz.sowul@pm.me>
2019-05-25 13:32:52 +0200

    Add login activity

--- app/src/main/java/com/example/cryptomemes/LoginActivity.kt
+++ app/src/main/java/com/example/cryptomemes/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.cryptomemes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -11,5 +12,17 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_btn.setOnClickListener {
+            val email = email_edittxt_log.text.toString()
+            val password = pass_edittxt_log.text.toString()
+
+            Log.d(TAG, "Email: $email")
+            Log.d(TAG, "Password: $password")
+        }
+
+        back2reg_txtview.setOnClickListener {
+            finish()
+        }
     }
 }
